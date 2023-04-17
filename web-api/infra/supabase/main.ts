59a640c1d4e473f6bcf4feaf11a5c892cf3a38d0
laionazeredo <laion.azeredo@gmail.com>
2023-04-17 18:38:06 -0300

    feat: adds dotenv support

--- web-api/infra/supabase/main.ts
+++ web-api/infra/supabase/main.ts
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types.ts'
 
+
 // Create a single supabase client for interacting with your database
-const supabase = createClient<Database>("https://wsucrybezkhzknwwwelt.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzdWNyeWJlemtoemtud3d3ZWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3NjA0NTEsImV4cCI6MTk5NzMzNjQ1MX0.GmmlFQAUTW7K4WXwkgW-8N4qKkXCz0ZtJ7nSYJCzDvA")
+const supabase = createClient<Database>(Deno.env.get('SUPABASE_URL'), Deno.env.get('SUPABASE_ANON_KEY'))
 
 export default supabase
--- web-api/server.ts
+++ web-api/server.ts
@@ -1,7 +1,8 @@
 import { serve } from "aleph/server";
 import denoDeploy from "aleph/plugins/deploy";
 import modules from "./routes/_export.ts";
-import "https://deno.land/x/dotenv/load.ts";
+import "https://deno.land/x/dotenv/load.ts"
+
 
 declare global {
   interface Context {
