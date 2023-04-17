3e7881e6e0c9bf776fe2decd319a71eced507806
laionazeredo <laion.azeredo@gmail.com>
2023-04-17 16:12:55 -0300

    feat: configures frameworks and deploy proocess

---
+++ web-api/server.ts
@@ -0,0 +1,25 @@
+import { serve } from "aleph/server";
+import denoDeploy from "aleph/plugins/deploy";
+import modules from "./routes/_export.ts";
+
+declare global {
+  interface Context {
+    foo: string;
+  }
+}
+
+serve({
+  plugins: [
+    denoDeploy({ modules }),
+  ],
+  port: 3000,
+  middlewares: [
+    {
+      name: "foo",
+      fetch: (_req, ctx) => {
+        ctx.foo = "bar";
+        return ctx.next();
+      },
+    },
+  ],
+});
