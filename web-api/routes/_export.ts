1fc9c24448a1471500c199092dee696ef7b1a7a8
laionazeredo <laion.azeredo@gmail.com>
2023-04-18 02:36:21 -0300

    feat: implements POST transactions/withdraw route

--- web-api/routes/_export.ts
+++ web-api/routes/_export.ts
@@ -4,12 +4,13 @@
 import * as $0 from "./index.ts";
 import * as $1 from "./users/index.ts";
 import * as $2 from "./transactions/investment.ts";
-import * as $3 from "./users/$id.ts";
-
+import * as $3 from "./transactions/withdraw.ts";
+import * as $4 from "./users/$id.ts";
 
 export default {
   "/": $0,
   "/users/index": $1,
   "/transactions/investment": $2,
-  "/users/:id": $3,
+  "/transactions/withdraw": $3,
+  "/users/:id": $4,
 };
