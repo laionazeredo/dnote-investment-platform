540b0ed05b05ca25f983ea14e30eba3142f69b0c
laionazeredo <laion.azeredo@gmail.com>
2023-04-18 00:58:31 -0300

    hotfix: remove unused var

--- web-api/routes/index.ts
+++ web-api/routes/index.ts
@@ -1,5 +1,5 @@
 // GET "/"
-export function GET(req: Request, ctx: Context) {
+export function GET(req: Request) {
   const url = new URL(req.url);
   return Response.json({
     "name": "dnote-web-api",
