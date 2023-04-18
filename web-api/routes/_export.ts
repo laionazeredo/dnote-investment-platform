109b0a0d147f44ccc8e945f862adb444512e61d9
laionazeredo <laion.azeredo@gmail.com>
2023-04-18 04:04:21 -0300

    feat: implements GET rates route

--- web-api/routes/_export.ts
+++ web-api/routes/_export.ts
@@ -5,14 +5,16 @@ import * as $0 from "./index.ts";
 import * as $1 from "./users/index.ts";
 import * as $2 from "./transactions/index.ts";
 import * as $3 from "./transactions/investment.ts";
-import * as $4 from "./transactions/withdraw.ts";
-import * as $5 from "./users/$id.ts";
+import * as $4 from "./transactions/rates.ts";
+import * as $5 from "./transactions/withdraw.ts";
+import * as $6 from "./users/$id.ts";
 
 export default {
   "/": $0,
   "/users/index": $1,
   "/transactions/index": $2,
   "/transactions/investment": $3,
-  "/transactions/withdraw": $4,
-  "/users/:id": $5,
+  "/transactions/rates": $4,
+  "/transactions/withdraw": $5,
+  "/users/:id": $6,
 };
---
+++ web-api/routes/transactions/rates.ts
@@ -0,0 +1,41 @@
+import supabase from "../../infra/supabase/main.ts";
+export async function GET(req: Request) {
+  const userId = req.url.split("?")[1].split("=")[1];
+  if (!userId) {
+    return Response.json({ data: null, error: "Invalid user id" }, {
+      status: 400,
+    });
+  }
+
+  const { data: accountData, error: userError } = await supabase
+    .from("Users")
+    .select("account_id")
+    .eq("id", userId);
+
+  if (userError) {
+    return Response.json({ data: null, error: userError }, { status: 500 });
+  }
+  if (accountData.length === 0) {
+    return Response.json({ data: null, error: "User not found" }, {
+      status: 400,
+    });
+  }
+
+  const { data: ratesData, error: ratesError } = await supabase
+    .from("Rates")
+    .select()
+    .eq("account_id", accountData[0].account_id);
+
+  if (ratesError) {
+    return Response.json({ data: null, error: ratesError });
+  }
+
+  return Response.json({
+    data: {
+      account_id: accountData[0].account_id,
+      rate: ratesData[0].monthly_rate,
+      updateRegime: "monthly",
+    },
+    error: ratesError,
+  }, { status: 200 });
+}
