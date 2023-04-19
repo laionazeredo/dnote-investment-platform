f6ed73851f97bac84dae62065d0734efb01b9529
laionazeredo <laion.azeredo@gmail.com>
2023-04-19 03:20:58 -0300

    feat: implements dockerfile support

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ export function GET(req: Request) {
   const url = new URL(req.url);
   return Response.json({
     "name": "dnote-web-api",
-    "version": "0.0.1",
+    "version": "1.0.0",
     "url": url.href,
   });
 }
